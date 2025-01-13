45540e1be3a3aac57a5a192b73f896f5efa9a8cd
2p5n <jdguerrero021422@gmail.com>
2025-01-13 00:20:29 -0500

    Corregido

--- Proyecto.scala
+++ Proyecto.scala
@@ -1,50 +1,72 @@
 import com.github.tototoshi.csv.{CSVReader, DefaultCSVFormat}
 
 object Proyecto {
-  @main
-  def main() : Unit = {
-    implicit object CSVFormatter extends  DefaultCSVFormat{
+  def main(args: Array[String]): Unit = {
+    implicit object CSVFormatter extends DefaultCSVFormat {
       override val delimiter: Char = ';'
     }
     val path = "C:\\Users\\jdgue\\OneDrive\\Documentos\\Universidad Oct-Feb 2025\\ProyectoIntegrador\\pi_movies_small.csv"
-
     val reader = CSVReader.open(path)
     val dataMap: List[Map[String, String]] = reader.allWithHeaders()
 
-    val listBudget: List[Long] = dataMap.
-      flatMap(row => row.get("budget").
-        flatMap(bdgt => scala.util.Try(bdgt.toLong).toOption))
-    val sumCount: Tuple2[Double, Int] =
-      listBudget
-        .foldLeft((0.0, 0))((t2, currVal) => (t2._1 + currVal, t2._2 + 1))
+    def EDA(columm: String): Unit = {
+      val list: List[Long] = dataMap.
+        flatMap(row => row.get(columm).
+          flatMap(bdgt => scala.util.Try(bdgt.toLong).toOption))
 
-    // Cálculo de la moda excluyendo 0
-    // Cálculo de la moda
-    val mode = listBudget
-      .filter(_ != 0)
-      .groupBy(x => x)
-      .map(t => (t._1, t._2.length))
-      .maxBy(_._2)
-      ._1
+      if (list.nonEmpty) {
+        val sumCount: (Double, Int) =
+          list.foldLeft((0.0, 0))((t2, currVal) => (t2._1 + currVal, t2._2 + 1))
 
-    val desviacionEstandar = math.sqrt(listBudget
-      .map(x => math.pow(x - sumCount._1 / sumCount._2, 2))
-      .sum / listBudget.length)
+        val mode = list
+          .filter(_ != 0)
+          .groupBy(x => x)
+          .map(t => (t._1, t._2.length))
+          .maxByOption(_._2)
+          .map(_._1)
+          .getOrElse(0L)
 
-    val avg = listBudget.filter(_>0).foldLeft(0.0, 0)((acc, currVal) => (acc._1 + currVal, acc._2 + 1))
+        val desviacionEstandar = math.sqrt(
+          list.map(x => math.pow(x - sumCount._1 / sumCount._2, 2)).sum / list.length
+        )
 
+        val avg = sumCount._1 / sumCount._2
+
+        println(s"min.: ${list.min}")
+        println(s"avg.: $avg")
+        println(s"max.: ${list.max}")
+        println(s"desviacion = $desviacionEstandar")
+        println(s"moda: $mode")
+      } else {
+        println(s"No hay datos válidos en la columna $columm.")
+      }
+
+
+
+  }
     val listAdult: List[String] = dataMap.flatMap(row => row.get("adult"))
     val falso = listAdult.groupBy(identity).map(t => (t._1, t._2.length))
+    println("Columna Adult: " + falso)
+    val listLanguage: List[String] = dataMap.flatMap(row => row.get("original_language"))
+    val language = listLanguage.groupBy(identity).map(t => (t._1, t._2.length))
+    println("Columna Language: " + language)
+    val listStatus: List[String] = dataMap.flatMap(row => row.get("status"))
+    val status = listStatus.groupBy(identity).map(t => (t._1, t._2.length))
+    println("Columna Status: " + status)
+    val listVideo: List[String] = dataMap.flatMap(row => row.get("video"))
+    val video = listVideo.groupBy(identity).map(t => (t._1, t._2.length))
+    println("Columna Video: " + video)
+    println("")
 
-    println(falso)
-    listAdult.distinct.foreach(println)
-    println(s"min.: ${listBudget.min}")
-    println(s"avg.: $avg")
-    println(s"max.: ${listBudget.max}")
-    println(s"desviacion = $desviacionEstandar")
-    println(s"moda: $mode")
-    reader.close()
+    println("Tabla Budget")
+    EDA("budget")
+    println("Tabla Revenue")
+    EDA("revenue")
+    println("Tabla Runtime")
+    EDA("runtime")
+    println("Tabla Vote_average")
+    EDA("vote_average")
+    println("Tabla Vote_count")
+    EDA("vote_count")
   }
 }
-
-
